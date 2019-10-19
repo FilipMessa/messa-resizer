97d96141f61346216723ac072780de07d4b934fa
Filip Messa <messa.filip@gmail.com>
2019-10-19 14:30:57 +0200

    refactor: improve eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+  "extends": ["wesbos"],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      ERROR,
+      {
+        devDependencies: ['.storybook/**', 'stories/**'],
+      },
+    ],
+  },
+}
